0261f1b5e7f959720707617f54c0e7fa14edeea2
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2024-12-30 20:18:14 +0500

    fix: fixed ps1 write error and rename vars to camelCase

--- update_domains.ps1
+++ update_domains.ps1
@@ -18,35 +18,37 @@ $domainsFilePath = $env:DOMAINS_FILE_PATH
 $localDomainsFile = $env:LOCAL_DOMAINS_FILE
 
 # Temporary files
-$remoteDomainsTemp = "remote_domains.txt"
-$allDomainsTemp = "all_domains.txt"
-$filteredDomainsTemp = "filtered_domains.txt"
+$tempRemoteDomains = "temp_remote_domains.txt"
+$tempNormalizedRemoteDomains = "temp_normalized_remote_domains.txt"
+$tempNormalizedLocalDomains = "temp_normalized_local_domains.txt"
+$tempAllDomains = "temp_all_domains.txt"
+$tempFilteredDomains = "temp_filtered_domains.txt"
 
 # Read the domain list from the router
-ssh -i $sshKeyPath "$routerUser@$routerHost" "cat $domainsFilePath" | Out-File $remoteDomainsTemp -Encoding utf8
+ssh -i $sshKeyPath "$routerUser@$routerHost" "cat $domainsFilePath" | Out-File $tempRemoteDomains -Encoding utf8
 
-# Normalize line endings to \n
-Get-Content $remoteDomainsTemp | ForEach-Object {$_ -replace "`r", ""} | Set-Content $remoteDomainsTemp
-Get-Content $localDomainsFile | ForEach-Object {$_ -replace "`r", ""} | Set-Content $localDomainsFile
+# Normalize line endings to \n and write to new temporary files
+Get-Content $tempRemoteDomains | ForEach-Object {$_ -replace "`r", ""} | Set-Content $tempNormalizedRemoteDomains
+Get-Content $localDomainsFile | ForEach-Object {$_ -replace "`r", ""} | Set-Content $tempNormalizedLocalDomains
 
 # Merge domain lists and remove duplicates and empty lines
-Get-Content $remoteDomainsTemp, $localDomainsFile | Where-Object {$_ -ne "" -and $_ -notmatch '^#'} | Sort-Object | Get-Unique | Out-File $allDomainsTemp -Encoding utf8
+Get-Content $tempNormalizedRemoteDomains, $tempNormalizedLocalDomains | Where-Object {$_ -ne "" -and $_ -notmatch '^#'} | Sort-Object | Get-Unique | Out-File $tempAllDomains -Encoding utf8
 
 # Remove domains listed in the local file with #
-$deleteDomains = Get-Content $localDomainsFile | Where-Object {$_ -match '^#'} | ForEach-Object {$_ -replace '^#'}
-Get-Content $allDomainsTemp | Where-Object {$_ -notin $deleteDomains} | Out-File $filteredDomainsTemp -Encoding utf8
+$deleteDomains = Get-Content $tempNormalizedLocalDomains | Where-Object {$_ -match '^#'} | ForEach-Object {$_ -replace '^#'}
+Get-Content $tempAllDomains | Where-Object {$_ -notin $deleteDomains} | Out-File $tempFilteredDomains -Encoding utf8
 
 # Read the updated domain list into a variable
-$updatedDomains = Get-Content $filteredDomainsTemp -Raw
+$updatedDomains = Get-Content $tempFilteredDomains -Raw
 
 # Send the updated domain list back to the router via SSH using echo
 ssh -i $sshKeyPath "$routerUser@$routerHost" "echo `"$updatedDomains`" > $domainsFilePath"
 
 # Save the updated domain list to the local file
-Copy-Item $filteredDomainsTemp $localDomainsFile
+Copy-Item $tempFilteredDomains $localDomainsFile
 
 # Execute the command to reload the homeproxy service
 ssh -i $sshKeyPath "$routerUser@$routerHost" "/etc/init.d/homeproxy reload"
 
 # Remove temporary files
-Remove-Item $remoteDomainsTemp, $allDomainsTemp, $filteredDomainsTemp
+Remove-Item $tempRemoteDomains, $tempNormalizedRemoteDomains, $tempNormalizedLocalDomains, $tempAllDomains, $tempFilteredDomains
--- update_domains.sh
+++ update_domains.sh
@@ -10,33 +10,35 @@ fi
 source .env
 
 # Temporary files
-remote_domains_temp="remote_domains.txt"
-all_domains_temp="all_domains.txt"
-filtered_domains_temp="filtered_domains.txt"
+tempRemoteDomains="temp_remote_domains.txt"
+tempNormalizedRemoteDomains="temp_normalized_remote_domains.txt"
+tempNormalizedLocalDomains="temp_normalized_local_domains.txt"
+tempAllDomains="temp_all_domains.txt"
+tempFilteredDomains="temp_filtered_domains.txt"
 
 # Read the domain list from the router via SSH
-ssh -i "$SSH_KEY_PATH" "$ROUTER_USER@$ROUTER_HOST" "cat $DOMAINS_FILE_PATH" > "$remote_domains_temp"
+ssh -i "$sshKeyPath" "$routerUser@$routerHost" "cat $domainsFilePath" > "$tempRemoteDomains"
 
 # Merge domain lists and remove empty lines and lines starting with #
-cat "$remote_domains_temp" | tr -d '\r' > "$remote_domains_temp.norm"
-cat "$LOCAL_DOMAINS_FILE" | tr -d '\r' > "$LOCAL_DOMAINS_FILE.norm"
+cat "$tempRemoteDomains" | tr -d '\r' > "$tempNormalizedRemoteDomains"
+cat "$localDomainsFile" | tr -d '\r' > "$tempNormalizedLocalDomains"
 
-cat "$remote_domains_temp.norm" "$LOCAL_DOMAINS_FILE.norm" | grep -v '^$' | grep -v '^#' | sort -u > "$all_domains_temp"
+cat "$tempNormalizedRemoteDomains" "$tempNormalizedLocalDomains" | grep -v '^$' | grep -v '^#' | sort -u > "$tempAllDomains"
 
 # Remove domains listed in the local file with #
-grep -vxf <(grep '^#' "$LOCAL_DOMAINS_FILE.norm" | sed 's/^#//') "$all_domains_temp" > "$filtered_domains_temp"
+grep -vxf <(grep '^#' "$tempNormalizedLocalDomains" | sed 's/^#//') "$tempAllDomains" > "$tempFilteredDomains"
 
 # Read the updated domain list into a variable
-updated_domains=$(cat "$filtered_domains_temp")
+updatedDomains=$(cat "$tempFilteredDomains")
 
 # Send the updated domain list back to the router via SSH using echo
-ssh -i "$SSH_KEY_PATH" "$ROUTER_USER@$ROUTER_HOST" "echo \"$updated_domains\" > $DOMAINS_FILE_PATH"
+ssh -i "$sshKeyPath" "$routerUser@$routerHost" "echo \"$updatedDomains\" > $domainsFilePath"
 
 # Save the updated domain list to the local file
-mv "$filtered_domains_temp" "$LOCAL_DOMAINS_FILE"
+mv "$tempFilteredDomains" "$localDomainsFile"
 
 # Execute the command to reload the homeproxy service
-ssh -i "$SSH_KEY_PATH" "$ROUTER_USER@$ROUTER_HOST" "/etc/init.d/homeproxy reload"
+ssh -i "$sshKeyPath" "$routerUser@$routerHost" "/etc/init.d/homeproxy reload"
 
 # Remove temporary files
-rm "$remote_domains_temp" "$remote_domains_temp.norm" "$LOCAL_DOMAINS_FILE.norm" "$all_domains_temp"
+rm "$tempRemoteDomains" "$tempNormalizedRemoteDomains" "$tempNormalizedLocalDomains" "$tempAllDomains"
